bab6213bc4935cc3cd763e85182a3ddde2e88a78
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-07-17 15:36:38 +0300

    Update mycode55.asm

--- mycode55.asm
+++ mycode55.asm
@@ -1,21 +1,18 @@
-
-    	ORG 	100h 
-    	JMP 	START	    ;	 
-X   	DW 	    04H      	; 
-SQ  	DW 	    ?        	; 
-CB  	DW 	    ?        	; 
-START:  MOV 	AX,X    	; 
-    	MOV 	BX,X    	; 
-    	MUL 	BX      	; 
-    	MOV 	SQ,AX   	; 
-    	MUL 	BX      	; 
-    	MOV 	CB,AX   	;     
-
-    	MOV 	AH,4CH  	; 
-    	INT 	21H     	; 
-    	RET 
+; Program to find square and cube of a number 
+    	ORG 	100h
+    	JMP 	START	; Jump to the beginning of the code	
+X   	DW 	    04H      	; Variable X = 4	
+SQ  	DW 	?        	; Variable SQ = square of X	
+CB  	DW 	?        	; Variable CB = cube of X	
+START:  MOV 	AX,X    	; Load X to AX	
+    	MOV 	BX,X    	; Load X to BX	
+    	MUL 	BX      	;Multiply AX and BX	
+    	MOV 	SQ,AX   	;Load square result to SQ	
+    	MUL 	BX      	;Multiply AX and BX	
+    	MOV 	CB,AX   	;Load cube of to CB	
     
-
-
+    	MOV 	AH,4CH  	;Terminate program	
+    	INT 	21H     	;Dos interrupt	
+    	RET
 
 
